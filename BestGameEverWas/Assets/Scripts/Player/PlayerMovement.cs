fad709aaccf947ff7adcce562d0acc4e69a73520
Nikita Sergejevs <78017695+Nikita-Sergejevs@users.noreply.github.com>
2022-06-03 18:09:29 +0300

    PlayerMovement

--- BestGameEverWas/Assets/Scripts/Player/PlayerMovement.cs
+++ BestGameEverWas/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed;
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
 
     public float groundDrag;
+
+    [Header("Crouching")]
+    private float startYScale;
+    public float crouchSpeed;
+    public float crouchYscale;
+
+    [Header("Jumping")]
+    public float jumpForce;
+    public float jumpCoolDown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("KeyBinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
     [Header("Ground Check")]
     public float playerHeight;
@@ -23,23 +42,39 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody rb;
 
+    public MovementState state;
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        ResetJump();
+
+        startYScale = transform.localScale.y;
     }
 
     private void Update()
     {
-        // groundChecck
+        // GroundChecck
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
         MyInput();
+        StateHandler();
+        MovePlayer();
 
-        // указателя перемещения
-        if(grounded)
+        // Указателя перемещения
+        if (grounded)
         {
             rb.drag = groundDrag;
-        }else
+        }
+        else
         {
             rb.drag = 0;
         }
@@ -54,13 +89,94 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        // Когда в прыжке
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCoolDown);
+        }
+        // Начал приседать
+        if (Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYscale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5, ForceMode.Impulse);
+        }
+        // Прекратил приседать
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
+    }
+
+    private void StateHandler()
+    {
+        // Когда присяд
+        if (grounded && Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+        // Когда бежит
+        else if (grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+        // Когда Идет
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+        // Когда в воздухе
+        else
+        {
+            state = MovementState.air;
+        }
+
     }
 
     private void MovePlayer()
     {
-        // рассчитать направление движения
+        // Рассчитать направление движения
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10, ForceMode.Force);
+        // На земле
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10, ForceMode.Force);
+        }
+        // В воздухе
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10 * airMultiplier, ForceMode.Force);
+        }
+
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, rb.velocity.z);
+
+        // Ограничиваем скорость если нам надо
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitdVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitdVel.x, rb.velocity.y, limitdVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // Убираем скрость Y
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
